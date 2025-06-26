bab92f6ac1b1f175f60f9e64c7d13a7dfa77d6ef
Christian Mollekopf <mollekopf@apheleia-it.ch>
2025-06-26 17:36:47 +0200

    Fixed database seed

--- config.demo/src/database/seeds/SharedFolderSeeder.php
+++ config.demo/src/database/seeds/SharedFolderSeeder.php
@@ -18,18 +18,22 @@ class SharedFolderSeeder extends Seeder
         $john = User::where('email', 'john@kolab.org')->first();
         $wallet = $john->wallets()->first();
 
-        $folder = SharedFolder::create([
-                'name' => 'Calendar',
-                'email' => 'folder-event@kolab.org',
-                'type' => 'event',
-        ]);
-        $folder->assignToWallet($wallet);
+        if (in_array("event", \config('app.shared_folder_types'))) {
+            $folder = SharedFolder::create([
+                    'name' => 'Calendar',
+                    'email' => 'folder-event@kolab.org',
+                    'type' => 'event',
+            ]);
+            $folder->assignToWallet($wallet);
+        }
 
-        $folder = SharedFolder::create([
-                'name' => 'Contacts',
-                'email' => 'folder-contact@kolab.org',
-                'type' => 'contact',
-        ]);
-        $folder->assignToWallet($wallet);
+        if (in_array("contact", \config('app.shared_folder_types'))) {
+            $folder = SharedFolder::create([
+                    'name' => 'Contacts',
+                    'email' => 'folder-contact@kolab.org',
+                    'type' => 'contact',
+            ]);
+            $folder->assignToWallet($wallet);
+        }
     }
 }
