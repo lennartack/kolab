5abdfbecaa871ed9148370203e1bc04f3812af07
Christian Mollekopf <mollekopf@apheleia-it.ch>
2025-08-20 10:20:12 +0200

    Publish swoole metrics without access control
    Summary:
    It's not sensitive information, and in normal scenarios we don't just
    make publicly available either.
    
    Differential Revision: https://git.kolab.org/D5553

--- src/routes/api.php
+++ src/routes/api.php
@@ -293,6 +293,14 @@ Route::group(
 if (\config('app.with_services')) {
     Route::group(
         [
+            'prefix' => 'webhooks',
+        ],
+        static function () {
+            Route::get('metrics/swoole', [API\V4\MetricsController::class, 'swooleMetrics']);
+        }
+    );
+    Route::group(
+        [
             'middleware' => ['allowedHosts'],
             'prefix' => 'webhooks',
         ],
@@ -303,7 +311,6 @@ if (\config('app.with_services')) {
             Route::post('cyrus-sasl', [API\V4\NGINXController::class, 'cyrussasl']);
 
             Route::get('metrics', [API\V4\MetricsController::class, 'metrics']);
-            Route::get('metrics/swoole', [API\V4\MetricsController::class, 'swooleMetrics']);
 
             Route::post('policy/greylist', [API\V4\PolicyController::class, 'greylist']);
             Route::post('policy/ratelimit', [API\V4\PolicyController::class, 'ratelimit']);
