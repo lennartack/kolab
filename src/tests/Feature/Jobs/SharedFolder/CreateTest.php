833828523f41404a63886a9f9b0c0f8226fb8a35
Aleksander Machniak <machniak@kolabsys.com>
2025-08-25 15:22:10 +0200

    Test improvement

--- src/tests/Feature/Jobs/SharedFolder/CreateTest.php
+++ src/tests/Feature/Jobs/SharedFolder/CreateTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Jobs\SharedFolder;
 
+use App\Domain;
 use App\Jobs\SharedFolder\CreateJob;
 use App\SharedFolder;
 use App\Support\Facades\IMAP;
@@ -21,6 +22,7 @@ class CreateTest extends TestCase
     protected function tearDown(): void
     {
         $this->deleteTestSharedFolder('folder-test@' . \config('app.domain'));
+        $this->deleteTestDomain('test.domain.tld');
 
         parent::tearDown();
     }
@@ -38,7 +40,7 @@ class CreateTest extends TestCase
         $job->assertReleased(delay: 5);
 
         $folder = $this->getTestSharedFolder(
-            'folder-test@' . \config('app.domain'),
+            'folder-test@test.domain.tld',
             ['status' => SharedFolder::STATUS_NEW]
         );
 
@@ -48,6 +50,24 @@ class CreateTest extends TestCase
 
         \config(['app.with_imap' => true]);
         \config(['app.with_ldap' => true]);
+
+        // Test domain does not exist
+        $job = (new CreateJob($folder->id))->withFakeQueueInteractions();
+        $job->handle();
+        $job->assertFailed();
+
+        $domain = $this->getTestDomain(
+            'test.domain.tld',
+            ['status' => Domain::STATUS_NEW, 'type' => Domain::TYPE_EXTERNAL]
+        );
+
+        // Test domain not LDAP ready
+        $job = (new CreateJob($folder->id))->withFakeQueueInteractions();
+        $job->handle();
+        $job->assertReleased(delay: 60);
+
+        $domain->status |= Domain::STATUS_LDAP_READY;
+        $domain->save();
 
         // Test shared folder creation
         IMAP::shouldReceive('createSharedFolder')->once()->with($folder)->andReturn(true);
@@ -70,7 +90,6 @@ class CreateTest extends TestCase
         $job->handle();
         $job->assertNotFailed();
 
-        // TODO: Test failures on domain sanity checks
         // TODO: Test partial execution, i.e. only IMAP or only LDAP
     }
 }
