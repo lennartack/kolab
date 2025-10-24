05ca5f8533bef13c6b245786144423b7925f10be
Christian Mollekopf <mollekopf@apheleia-it.ch>
2025-10-24 12:59:49 +0200

    Amavis fixup

--- docker/amavis/rootfs/init.sh
+++ docker/amavis/rootfs/init.sh
@@ -22,6 +22,7 @@ sed -i -r \
     -e "s|DB_HOST|$DB_HOST|g" \
     -e "s|DB_DATABASE|$DB_DATABASE|g" \
     -e "s|DB_USERNAME|$DB_USERNAME|g" \
+    -e "s|DB_PASSWORD|$DB_PASSWORD|g" \
     -e "s|SPAM_DETECTED_LEVEL|$SPAM_DETECTED_LEVEL|g" \
     -e "s|SPAM_KILL_LEVEL|$SPAM_KILL_LEVEL|g" \
     -e "s|SPAM_KILL_ACTION|$SPAM_KILL_ACTION|g" \
