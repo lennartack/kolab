58c2b988a55b30cb2159d5fd926c2033cb4e77fb
Christian Mollekopf <mollekopf@apheleia-it.ch>
2025-10-28 16:55:15 +0100

    Reduce the chunk size so we don't go out of memory
    Summary: I ran into OOM issues with 512M.
    
    Differential Revision: https://git.kolab.org/D5694

--- src/app/DataMigrator/Driver/IMAP.php
+++ src/app/DataMigrator/Driver/IMAP.php
@@ -17,7 +17,7 @@ use App\User;
 class IMAP implements ExporterInterface, ImporterInterface
 {
     /** @var int Max number of items to migrate in one go */
-    protected const CHUNK_SIZE = 100;
+    protected const CHUNK_SIZE = 50;
 
     /** @var ?\rcube_imap_generic Imap backend */
     protected $imap;
