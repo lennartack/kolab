20001c085642f9dbfacb5e805a596e6ed6974fc1
Christian Mollekopf <mollekopf@apheleia-it.ch>
2026-01-22 14:43:07 +0100

    Use LIBXML_PARSEHUGE to successfully parse large attachments.
    libxml will silently truncate the node internally, and only log an
    error with libxml_use_internal_errors enabled. We thus use that now in
    debug mode at least, so we have a chance to find such issues.
    Setting LIBXML_PARSEHUGE fixes loading of large nodes though.

--- src/app/Backends/DAV.php
+++ src/app/Backends/DAV.php
@@ -837,7 +837,7 @@ class DAV
         $doc = new \DOMDocument('1.0', 'UTF-8');
 
         if (str_starts_with($xml, '<?xml')) {
-            if (!$doc->loadXML($xml)) {
+            if (!$doc->loadXML($xml, LIBXML_PARSEHUGE)) {
                 throw new \Exception("Failed to parse XML");
             }
 
@@ -872,9 +872,16 @@ class DAV
             $doc->formatOutput = true;
             $doc->preserveWhiteSpace = false;
 
-            if (!$doc->loadXML($body)) {
+            libxml_use_internal_errors(true);
+
+            if (!$doc->loadXML($body, LIBXML_PARSEHUGE)) {
                 throw new \Exception("Failed to parse XML");
             }
+
+            foreach (libxml_get_errors() as $error) {
+                \Log::error(trim($error->message) . " on line " . $error->line . PHP_EOL);
+            }
+            libxml_clear_errors();
 
             $body = $doc->saveXML();
         }
