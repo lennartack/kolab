04e5bda21bc5e21d9c9301178157343ab3b2bbca
Lennart Ackermans <lennart@ackermans.ch>
2026-04-12 05:08:43 +0200

    Add Cyrus build flag to avoid compiler error

--- docker/imap/build.sh
+++ docker/imap/build.sh
@@ -27,7 +27,11 @@ autoreconf -i
 # imap/httpd.c: In function ‘log_request’:
 # imap/httpd.c:2974:9: error: ‘noargs’ may be used uninitialized [-Werror=maybe-uninitialized]
 #  2974 |         comma_list_body(logbuf, upgrd_tokens, txn->flags.upgrade, 0, noargs);
-./configure CFLAGS="-W -Wno-unused-parameter -g -O0 -Wall -Wextra -Werror -Wno-error=deprecated-declarations  -Wno-error=free-nonheap-object -Wno-error=type-limits -Wno-error=maybe-uninitialized -fPIC" --enable-murder --enable-http --enable-calalarmd --enable-autocreate --enable-idled --with-openssl=yes --enable-replication --prefix=/usr
+# -Wno-error=format for the following on arm64:
+# imap/cyr_ls.c:185:36: error: format '%lu' expects argument of type
+# 'long unsigned int', but argument 12 has type '__nlink_t' {aka
+# 'unsigned int'} [-Werror=format=]
+./configure CFLAGS="-W -Wno-unused-parameter -g -O0 -Wall -Wextra -Werror -Wno-error=deprecated-declarations  -Wno-error=free-nonheap-object -Wno-error=type-limits -Wno-error=maybe-uninitialized -Wno-error=format -fPIC" --enable-murder --enable-http --enable-calalarmd --enable-autocreate --enable-idled --with-openssl=yes --enable-replication --prefix=/usr
 
 
 make -j6
